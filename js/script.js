1214225e288a95fed20e68e76a8ec491e754135f
SPARSH BANSAL <78899820+SparshB21@users.noreply.github.com>
2022-09-06 23:27:58 +0530

    Validate update

--- js/script.js
+++ js/script.js
@@ -1,12 +1,86 @@
-let menu = document.querySelector('#menu-btn');
-let navbar = document.querySelector('.navbar');
+var nameError = document.getElementById('name-error');
+var phoneError = document.getElementById('phone-error');
+var emailError = document.getElementById('email-error');
+var messageError = document.getElementById('message-error');
+var submitError = document.getElementById('submit-error');
 
-menu.onclick = () =>{
-    menu.classList.toggle('fa-times');
-    navbar.classList.toggle('active');
+function validateName(){
+    var name = document.getElementById('contact-name').value;
+
+    if(name.length==0){
+        nameError.innerHTML = 'Name is required';
+        return false;
+    }
+
+    if(!name.match(/^[A-Za-z]*\s{1}[A-Za-z]*$/)){
+        nameError.innerHTML = 'Write full name';
+        return false;
+    }
+
+    nameError.innerHTML = '<i class="fas fa-circle-check"></i>';
+    return true;
 }
 
-window.onscroll = () =>{
-    menu.classList.remove('fa-times');
-    navbar.classList.remove('active');
+function validatePhone(){
+    var phone = document.getElementById('contact-phone').value;
+
+    if(phone.length==0)
+    {phoneError.innerHTML = 'Phone no. is required';
+    return false;
+}
+
+    if(phone.length!=10){
+        phoneError.innerHTML = 'Phone no. should be 10 digits';
+        return false;
+    }
+
+    if (!phone.match(/^[0-9]{10}$/)){
+        phoneError.innerHTML = 'Only digits please';
+        return false;
+    }
+
+    phoneError.innerHTML = 'okk';
+    return true;
+
+}
+
+function validateEmail(){
+    var email = document.getElementById('contact-email').value;
+
+    if(email.length == 0){
+        emailError.innerHTML = 'Email is required';
+        return false;
+    }
+
+    if(!email.match(/^[A-Za-z\._\-[0-9]*[@][A-Za-z]*[/.][a-z]{2,4}$/)){
+        emailError.innerHTML = 'Email Invalid';
+        return false;
+    }
+
+    emailError.innerHTML = 'okkk';
+    return true;
+}
+
+function validateMessage(){
+    var message = document.getElementById('contact-message').value;
+    var required = 30;
+    var left = required - message.length;
+
+    if(left>0){
+        messageError.innerHTML = left + ' more characters required';
+        return false;
+    }
+
+    messageError.innerHTML = 'okkkk';
+    return true;
+
+}
+
+function validateForm(){
+    if(!validateName() || !validatePhone() || !validateEmail() || !validateMessage()){
+        submitError.style.display = 'block';
+        submitError.innerHTML = 'Please fix error to submit';
+        setTimeout(function(){submitError.style.display = 'none';}, 3000);
+        return false;
+    }
 }

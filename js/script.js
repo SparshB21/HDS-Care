40217056ca3ad6f5ab83ecb6d4c2940dcc95fafa
SPARSH BANSAL <78899820+SparshB21@users.noreply.github.com>
2022-09-08 19:30:43 +0530

    Latest Update

--- js/script.js
+++ js/script.js
@@ -3,21 +3,25 @@ var phoneError = document.getElementById('phone-error');
 var emailError = document.getElementById('email-error');
 var messageError = document.getElementById('message-error');
 var submitError = document.getElementById('submit-error');
+var fieldname = document.getElementById('field-name');
 
 function validateName(){
     var name = document.getElementById('contact-name').value;
 
     if(name.length==0){
         nameError.innerHTML = 'Name is required';
+        nameError.style.color = 'red';
         return false;
     }
 
     if(!name.match(/^[A-Za-z]*\s{1}[A-Za-z]*$/)){
         nameError.innerHTML = 'Write full name';
+        nameError.style.color = 'red';
         return false;
     }
 
-    nameError.innerHTML = '<i class="fas fa-circle-check"></i>';
+    nameError.innerHTML = '✔';
+    nameError.style.color = '#16a085';
     return true;
 }
 
@@ -26,11 +30,13 @@ function validatePhone(){
 
     if(phone.length==0)
     {phoneError.innerHTML = 'Phone no. is required';
+    phoneError.style.color = 'red';
     return false;
 }
 
     if(phone.length!=10){
         phoneError.innerHTML = 'Phone no. should be 10 digits';
+        phoneError.style.color = 'red';
         return false;
     }
 
@@ -39,7 +45,9 @@ function validatePhone(){
         return false;
     }
 
-    phoneError.innerHTML = 'okk';
+    phoneError.innerHTML = '✔';
+    phoneError.style.color = '#16a085';
+
     return true;
 
 }
@@ -49,15 +57,19 @@ function validateEmail(){
 
     if(email.length == 0){
         emailError.innerHTML = 'Email is required';
+        emailError.style.color = 'red';
         return false;
     }
 
     if(!email.match(/^[A-Za-z\._\-[0-9]*[@][A-Za-z]*[/.][a-z]{2,4}$/)){
         emailError.innerHTML = 'Email Invalid';
+        emailError.style.color = 'red';
         return false;
     }
 
-    emailError.innerHTML = 'okkk';
+    emailError.innerHTML = '✔';
+    emailError.style.color = '#16a085';
+
     return true;
 }
 
@@ -68,10 +80,12 @@ function validateMessage(){
 
     if(left>0){
         messageError.innerHTML = left + ' more characters required';
+        messageError.style.color = 'red';
         return false;
     }
 
-    messageError.innerHTML = 'okkkk';
+    messageError.innerHTML = '✔';
+    messageError.style.color = '#16a085';
     return true;
 
 }
@@ -79,8 +93,15 @@ function validateMessage(){
 function validateForm(){
     if(!validateName() || !validatePhone() || !validateEmail() || !validateMessage()){
         submitError.style.display = 'block';
+        submitError.style.color = 'red';
         submitError.innerHTML = 'Please fix error to submit';
         setTimeout(function(){submitError.style.display = 'none';}, 3000);
         return false;
     }
 }
+
+// function validateField(){
+//    var field = document.getElementById('field-name').value;
+
+//    alert(field + 'is your field !!');
+// }
